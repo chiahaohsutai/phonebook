029c9ba1f73bb65ccbd499150ce30cc2b5b5e301
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-04-17 20:55:50 -0400

    new pages

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, redirect, request, render_template
-from db.handlers import read
+from flask import Flask, flash, redirect, render_template, request
+from uuid import uuid4
+
+from db.handlers import insert, read
+from db.schemas import Contact
 
 app = Flask(__name__)
+app.secret_key = uuid4().bytes
 route = app.route
+
 
 @route("/")
 def index():
@@ -18,3 +23,24 @@ def contacts():
         contacts = read({"$or": conditions})
 
     return render_template("index.html", contacts=contacts)
+
+
+@route("/contacts/new", methods=["GET"])
+def contacts_new_get():
+    return render_template("new.html", contact=Contact())
+
+
+@route("/contacts/new", methods=["POST"])
+def contact_new_post():
+    form = request.form
+    new_contact = Contact(**form.to_dict())
+    if insert(new_contact):
+        flash("Contact added successfully!", "success")
+        return redirect("/contacts")
+    else:
+        return render_template("new.html", contact=new_contact)
+
+@route("/contacts/<contact_id>")
+def contacts_view(contact_id: str):
+    contact = read({"_id": contact_id})
+    return render_template("show.html", contact=contact[0])
