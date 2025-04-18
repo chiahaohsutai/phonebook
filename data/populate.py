3407296eb2652d0ef3db9fbc90c30bd48f28f787
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-04-17 20:55:31 -0400

    updated populate script

--- data/populate.py
+++ data/populate.py
@@ -3,6 +3,7 @@ import json
 import logging
 import pathlib
 from os import getcwd
+from uuid import uuid4
 
 from pymongo import AsyncMongoClient
 
@@ -21,10 +22,16 @@ async def populate():
         file_path = pathlib.Path(getcwd()) / "data" / "contacts.json"
         with open(file_path, "r") as file:
             data = json.load(file)
+        
+        for item in data:
+            item["_id"] = str(uuid4())
+            item.pop("errors", None)
+            item.pop("id", None)
 
         result = await collection.insert_many(data)
 
         if result.acknowledged:
+            await collection.create_index({ "email": 1 }, unique=True)
             logger.info("Data inserted successfully.")
         else:
             logger.error("Failed to insert data.")
